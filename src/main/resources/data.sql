95f12a286eb047caab251e844a2f41dfb079474d
YuxRRJ <fa90502@gmail.com>
2022-06-09 21:52:11 +0800

    h2 init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into coin_chinese_Table values(
+'USD',
+'美金'
+);
+insert into coin_chinese_Table values(
+'GBP',
+'英鎊'
+);
+insert into coin_chinese_Table values(
+'EUR',
+'歐元'
+);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE coin_chinese_Table(
+coinType char(20),
+chineseName char(20)
+);
